8148947392ca90def78215ba76c9be9445fe543c
barbaracalderon <bcalderoni.ti@gmail.com>
2024-05-14 15:31:15 -0300

    feat: 404 page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'skills', component: SkillsComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotfoundComponent }
 ];
