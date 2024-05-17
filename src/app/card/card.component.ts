1b1e18f474be6e87cd1a4d9d4db3b889ce6a4b0e
barbaracalderon <bcalderoni.ti@gmail.com>
2024-05-17 00:22:46 -0300

    feat: correct import of BsModalService and BsModalRef inside card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { Card } from '../_models/card';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
+  providers: [BsModalService],
 })
+
 export class CardComponent {
   @Input() card = {} as Card;
+  bsModalRef? : BsModalRef;
+
+  constructor(private modalService: BsModalService) {
+  }
+
+  OpenCardModal() {
+    this.bsModalRef = this.modalService.show('');
+  }
 }
