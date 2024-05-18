b8132f82db58c1acc0871bbc1aeb21de716c9174
barbaracalderon <bcalderoni.ti@gmail.com>
2024-05-18 15:47:34 -0300

    feat: add footer currentYear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
