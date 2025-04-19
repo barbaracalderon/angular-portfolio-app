dfd08728b40e5dc7e5f5efb06757ddd511c49f93
barbaracalderon <bcalderoni.ti@gmail.com>
2025-04-19 15:09:16 -0300

    test: update AppComponent test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot(
+          [
+                { path: '', component: AppComponent },
+                { path: '', component: HeaderComponent },
+                { path: '', component: ContentComponent },
+                { path: '', component: FooterComponent }
+          ]
+        )
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'angular-portfolio-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-portfolio-app');
+  it('should contain the header component', () => {
+    const header = fixture.debugElement.query(By.css('app-header'));
+    expect(header).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-portfolio-app');
+  it('should contain the content component', () => {
+    const content = fixture.debugElement.query(By.css('app-content'));
+    expect(content).toBeTruthy();
+  });
+
+  it('should contain the footer component', () => {
+    const footer = fixture.debugElement.query(By.css('app-footer'));
+    expect(footer).toBeTruthy();
+  });
+
+  it('should have the correct component structure', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-header')).toBeTruthy();
+    expect(compiled.querySelector('app-content')).toBeTruthy();
+    expect(compiled.querySelector('app-footer')).toBeTruthy();
   });
 });
