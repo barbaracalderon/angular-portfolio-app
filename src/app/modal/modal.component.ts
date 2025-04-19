e1271f26185e1a46a431f00e209ed3a8ce3efce8
barbaracalderon <bcalderoni.ti@gmail.com>
2025-04-19 11:42:16 -0300

    refactor(carousel): upgrade v19 to swiper

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, AfterViewInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { register } from 'swiper/element/bundle';
 import { SwiperOptions } from 'swiper/types';
@@ -9,15 +9,18 @@ import { MatDividerModule } from '@angular/material/divider';
 import { Card } from '../_models/card';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+
 register();
 
 @Component({
   selector: 'app-modal',
   standalone: true,
   templateUrl: './modal.component.html',
-  styleUrls: [
-    './modal.component.css',
-  ],
+  styleUrls: ['./modal.component.css'],
   imports: [
     MatIconModule,
     MatButtonModule,
@@ -26,12 +29,28 @@ register();
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class ModalComponent {
+export class ModalComponent implements AfterViewInit {
   card: Card;
   swiperConfig: SwiperOptions = {
-    pagination: { clickable: true },
-    navigation: true,
-    loop: true
+    pagination: {
+      clickable: true,
+      type: 'bullets',
+      renderBullet: (index, className) => {
+        return `<span class="${className} swiper-pagination-bullet-custom"></span>`;
+      }
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    }
   };
 
   constructor(
@@ -39,5 +58,13 @@ export class ModalComponent {
     @Inject(MAT_DIALOG_DATA) public data: { card: Card }
   ) {
     this.card = data.card;
+  }
+
+  ngAfterViewInit() {
+    const swiperEl = document.querySelector('swiper-container');
+    if (swiperEl) {
+      Object.assign(swiperEl, this.swiperConfig);
+      swiperEl.initialize();
+    }
   }
 }
